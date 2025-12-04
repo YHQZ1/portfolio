f22406b5c6ce19726a39a05b71488b656df467e3
Uttkarsh <rupareluttkarsh2309@gmail.com>
2025-12-05 00:48:07 +0530

    scroll button fix

--- src/components/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton.jsx
@@ -22,20 +22,20 @@ export default function ScrollToTopButton({ darkMode }) {
     <button
       onClick={scrollToTop}
       className={`
-        fixed bottom-6 right-6 z-50 p-3 rounded-full shadow-md
-        transition-none duration-300 hover:scale-[1.1]
-        ${
-          visible
-            ? "opacity-100 pointer-events-auto"
-            : "opacity-0 pointer-events-none"
-        }
+    fixed bottom-6 right-6 z-50 p-3 rounded-full shadow-md
+    transition-colors duration-200
+    ${
+      visible
+        ? "opacity-100 pointer-events-auto"
+        : "opacity-0 pointer-events-none"
+    }
 
-        ${
-          darkMode
-            ? "bg-white text-black border-white"
-            : "bg-black text-white border-black"
-        }
-      `}
+    ${
+      darkMode
+        ? "bg-white text-black border-white hover:bg-[#e5e5e5]"
+        : "bg-black text-white border-black hover:bg-[#222]"
+    }
+  `}
     >
       <FaArrowUp className="text-lg" />
     </button>
