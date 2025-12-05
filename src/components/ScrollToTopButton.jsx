61acb9eff65a261266e40111ebe5042b5e305f68
Uttkarsh <rupareluttkarsh2309@gmail.com>
2025-12-05 13:11:39 +0530

    minor fix

--- src/components/ScrollToTopButton.jsx
+++ src/components/ScrollToTopButton.jsx
@@ -23,7 +23,7 @@ export default function ScrollToTopButton({ darkMode }) {
       onClick={scrollToTop}
       className={`
     fixed bottom-6 right-6 z-50 p-3 rounded-full shadow-md
-    transition-colors duration-200
+    transition-none duration-200
     ${
       visible
         ? "opacity-100 pointer-events-auto"
